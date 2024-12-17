1a9e70ccd1ac98b200fe807643ee1d543986e0e9
Mehreen1 <mehreenyasir91@gmail.com>
2024-12-17 16:51:59 +0500

    changes in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,6 +9,8 @@ const Header = () => {
        <Logo/>
       <nav>
         <ul>
+
+          
           <li><a href="./home">Home</a></li>
           <li><a href="./products"> Products </a></li>
           <li><a href="./about">About</a></li>
